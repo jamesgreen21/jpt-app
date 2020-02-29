ad350b7108b3f819e17d4039c0ad9fffde5ff6f7
Ubuntu <ubuntu@ip-172-31-91-241.ec2.internal>
2020-02-29 19:19:22 +0000

    Fixes made to Media Files on form upload

--- health_blog/models.py
+++ health_blog/models.py
@@ -12,16 +12,11 @@ class Blog(models.Model):
     headline = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(
-        upload_to='blog_pics',
-        default='blog_pics/default.jpeg',
+        upload_to='blog_img',
+        default='blog_img/default.jpg',
     )
     created_date = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Blog, self).save(*args, **kwargs)
