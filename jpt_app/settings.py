43ce311d68f19d773d49c1bc07d912c1ea97bd68
jamesgreen21 <44775216+jamesgreen21@users.noreply.github.com>
2020-03-23 17:43:53 +0000

    Update settings.py

--- jpt_app/settings.py
+++ jpt_app/settings.py
@@ -181,7 +181,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
